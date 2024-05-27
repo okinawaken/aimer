47dc97e0750989e38b78c24c98e482fc3759eebb
Shinomiya <1138700280@qq.com>
2024-05-27 19:42:16 +0800

    init new module

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # aimer
 aimer is a Java EE enterprise-level fast development platform.
+
+# environment
+--spring.cloud.polaris.address=
+--spring.cloud.polaris.namespace=
+--spring.cloud.tencent.metadata.content.env=
+--spring.profiles.active=
+--spring.application.name=
---
+++ aimer-console/src/main/java/org/dromara/aimer/console/AimerApplication.java
@@ -0,0 +1,14 @@
+package org.dromara.aimer.console;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimerApplication.class, args);
+    }
+
+}
