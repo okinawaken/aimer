33a1c1641cd6f3a47e55b95e38c5207606869da5
Shinomiya <1138700280@qq.com>
2024-05-29 19:41:39 +0800

    引入hutool

---
+++ aimer-repository/src/main/java/org/dromara/aimer/repository/AimerRepositoryContext.java
@@ -0,0 +1,9 @@
+package org.dromara.aimer.repository;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan
+public class AimerRepositoryContext {
+}
--- aimer-service/src/main/java/org/dromara/aimer/service/AimerServiceContext.java
+++ aimer-service/src/main/java/org/dromara/aimer/service/AimerServiceContext.java
@@ -1,9 +1,12 @@
 package org.dromara.aimer.service;
 
+import org.dromara.aimer.repository.AimerRepositoryContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
 @Component
 @ComponentScan
+@Import(AimerRepositoryContext.class)
 public class AimerServiceContext {
 }
