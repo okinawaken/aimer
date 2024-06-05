ad6c85a7b132f7fedd44873b64ab3b30814c107c
Shinomiya <1138700280@qq.com>
2024-06-05 14:28:25 +0800

    添加枚举

---
+++ aimer-common/src/main/java/org/dromara/aimer/common/enums/OperatorType.java
@@ -0,0 +1,21 @@
+package org.dromara.aimer.common.enums;
+
+/**
+ * 操作人类别
+ */
+public enum OperatorType {
+    /**
+     * 其它
+     */
+    OTHER,
+
+    /**
+     * 后台用户
+     */
+    MANAGE,
+
+    /**
+     * 手机端用户
+     */
+    MOBILE
+}
