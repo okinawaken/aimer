2bf361db14c4768745e22ee556d516092ac8a9c4
Shinomiya <1138700280@qq.com>
2024-06-05 15:05:37 +0800

    增加校验分组

---
+++ aimer-common/src/main/java/org/dromara/aimer/common/validate/AddGroup.java
@@ -0,0 +1,7 @@
+package org.dromara.aimer.common.validate;
+
+/**
+ * 校验分组 add
+ */
+public interface AddGroup {
+}
---
+++ aimer-common/src/main/java/org/dromara/aimer/common/validate/EditGroup.java
@@ -0,0 +1,7 @@
+package org.dromara.aimer.common.validate;
+
+/**
+ * 校验分组 edit
+ */
+public interface EditGroup {
+}
