54fcd847d71e345a82a98092bae90eb70718d34d
Shinomiya <1138700280@qq.com>
2024-06-18 14:37:03 +0800

    优化配置读取

--- aimer-service/src/main/java/org/dromara/aimer/service/config/AbstractConfig.java
+++ aimer-service/src/main/java/org/dromara/aimer/service/config/AbstractConfig.java
@@ -4,22 +4,15 @@ import com.mendmix.common.util.BeanUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.aimer.common.utils.JsonUtils;
 import org.dromara.aimer.common.utils.ValidatorUtils;
-import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 public abstract class AbstractConfig {
-
-    @Value("${spring.profiles.active}")
-    protected String namespace;
-
-    @Value("${spring.application.name}")
-    protected String fileGroup;
 
     protected void refreshConfig(String configStr) {
         // 监听对象转换
         AbstractConfig newConfig = JsonUtils.toObject(configStr, this.getClass());
         log.info("监听到配置变更，namespace：{}，fileGroup：{}，fileName：{}，oldConfig：{}，newConfig：{}",
-                namespace, fileGroup, fileName(), JsonUtils.toJson(this), JsonUtils.toJson(newConfig));
+                namespace(), group(), fileName(), JsonUtils.toJson(this), JsonUtils.toJson(newConfig));
 
         // 校验配置对象
         ValidatorUtils.validate(newConfig);
@@ -27,6 +20,10 @@ public abstract class AbstractConfig {
         // 更新配置对象
         BeanUtils.copy(newConfig, this, true);
     }
+
+    abstract String namespace();
+
+    abstract String group();
 
     abstract String fileName();
 }
--- aimer-service/src/main/java/org/dromara/aimer/service/config/AbstractPolarisConfig.java
+++ aimer-service/src/main/java/org/dromara/aimer/service/config/AbstractPolarisConfig.java
@@ -1,9 +1,12 @@
 package org.dromara.aimer.service.config;
 
+import com.tencent.cloud.polaris.context.config.PolarisContextProperties;
 import com.tencent.polaris.configuration.api.core.ConfigFile;
 import com.tencent.polaris.configuration.api.core.ConfigFileService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
@@ -13,14 +16,31 @@ public abstract class AbstractPolarisConfig extends AbstractConfig {
     @Autowired
     private ConfigFileService configFileService;
 
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private PolarisContextProperties polarisContextProperties;
+
     @PostConstruct
     public void init() {
-        ConfigFile configFile = configFileService.getConfigFile(namespace, fileGroup, fileName());
+        ConfigFile configFile = configFileService.getConfigFile(namespace(), group(), fileName());
         refreshConfig(configFile.getContent());
 
         configFile.addChangeListener(event -> {
             String newValue = event.getNewValue();
             refreshConfig(newValue);
         });
+    }
+
+    @Override
+    String namespace() {
+        return polarisContextProperties.getNamespace();
+    }
+
+    @Override
+    String group() {
+        return StringUtils.getIfBlank(polarisContextProperties.getService(),
+                () -> environment.getProperty("spring.application.name"));
     }
 }
