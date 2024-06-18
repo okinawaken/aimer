eb262e49f48b11d5cdb12711075d7d8de0bf2819
Shinomiya <1138700280@qq.com>
2024-06-18 15:36:33 +0800

    ratelimiter

--- aimer-service/src/main/java/org/dromara/aimer/service/configuration/ratelimiter/aspectj/RateLimiterAspect.java
+++ aimer-service/src/main/java/org/dromara/aimer/service/configuration/ratelimiter/aspectj/RateLimiterAspect.java
@@ -1,12 +1,16 @@
 package org.dromara.aimer.service.configuration.ratelimiter.aspectj;
 
+import cn.hutool.core.util.ArrayUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.dromara.aimer.common.exception.ServiceException;
 import org.dromara.aimer.common.utils.MessageUtils;
+import org.dromara.aimer.common.utils.ServletUtils;
+import org.dromara.aimer.repository.constants.GlobalConstants;
 import org.dromara.aimer.repository.utils.RedisUtils;
 import org.dromara.aimer.service.configuration.ratelimiter.annotation.RateLimiter;
 import org.dromara.aimer.service.enums.LimitType;
@@ -14,11 +18,14 @@ import org.redisson.api.RateType;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.ParserContext;
 import org.springframework.expression.common.TemplateParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.lang.reflect.Method;
 
 /**
  * 限流处理
@@ -77,7 +84,46 @@ public class RateLimiterAspect {
     }
 
     public String getCombineKey(RateLimiter rateLimiter, JoinPoint point) {
-        // todo 实现
-        return "";
+        String key = rateLimiter.key();
+        // 获取方法(通过方法签名来获取)
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        Class<?> targetClass = method.getDeclaringClass();
+        // 判断是否是spel格式
+        if (StringUtils.containsAny(key, "#")) {
+            // 获取参数值
+            Object[] args = point.getArgs();
+            // 获取方法上参数的名称
+            String[] parameterNames = pnd.getParameterNames(method);
+            if (ArrayUtil.isEmpty(parameterNames)) {
+                throw new ServiceException("限流key解析异常!请联系管理员!");
+            }
+            for (int i = 0; i < parameterNames.length; i++) {
+                context.setVariable(parameterNames[i], args[i]);
+            }
+            // 解析返回给key
+            try {
+                Expression expression;
+                if (StringUtils.startsWith(key, parserContext.getExpressionPrefix())
+                        && StringUtils.endsWith(key, parserContext.getExpressionSuffix())) {
+                    expression = parser.parseExpression(key, parserContext);
+                } else {
+                    expression = parser.parseExpression(key);
+                }
+                key = expression.getValue(context, String.class) + ":";
+            } catch (Exception e) {
+                throw new ServiceException("限流key解析异常!请联系管理员!");
+            }
+        }
+        StringBuilder stringBuffer = new StringBuilder(GlobalConstants.RATE_LIMIT_KEY);
+        stringBuffer.append(ServletUtils.getRequest().getRequestURI()).append(":");
+        if (rateLimiter.limitType() == LimitType.IP) {
+            // 获取请求ip
+            stringBuffer.append(ServletUtils.getClientIP()).append(":");
+        } else if (rateLimiter.limitType() == LimitType.CLUSTER) {
+            // 获取客户端实例id
+            stringBuffer.append(RedisUtils.getClient().getId()).append(":");
+        }
+        return stringBuffer.append(key).toString();
     }
 }
