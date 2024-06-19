35cb7c378bae27c824f0417abe9d404eb84077b4
Shinomiya <1138700280@qq.com>
2024-06-19 15:30:06 +0800

    增加ConsoleExceptionHandler

--- aimer-console/src/main/java/org/dromara/aimer/console/configuration/dubbo/DubboConfiguration.java
+++ aimer-console/src/main/java/org/dromara/aimer/console/configuration/dubbo/DubboConfiguration.java
@@ -1,4 +1,4 @@
-package org.dromara.aimer.console.configuration;
+package org.dromara.aimer.console.configuration.dubbo;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.Configuration;
