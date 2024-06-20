5c759c0b4aa4db9c47fe9f1a52fd862c7b5ebf10
Shinomiya <1138700280@qq.com>
2024-06-20 17:42:04 +0800

    优化integration

--- aimer-integration/src/main/java/org/dromara/aimer/integration/AimerIntegrationContext.java
+++ aimer-integration/src/main/java/org/dromara/aimer/integration/AimerIntegrationContext.java
@@ -3,6 +3,9 @@ package org.dromara.aimer.integration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author bixiu
+ */
 @Component
 @ComponentScan
 public class AimerIntegrationContext {
--- aimer-integration/src/main/java/org/dromara/aimer/integration/dubbo/aimer/GreetingDubboRpcService.java
+++ aimer-integration/src/main/java/org/dromara/aimer/integration/dubbo/aimer/GreetingDubboRpcService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+/**
+ * @author bixiu
+ */
 @Slf4j
 @Service
 public class GreetingDubboRpcService {
@@ -20,16 +23,16 @@ public class GreetingDubboRpcService {
     public String sayHello(String name) {
         try {
             BaseResponse<String> response = greetingRpcService.sayHello(name);
-            log.info("GreetingDubboRpcService sayHello, name:{}, response:{}" , name, JsonUtils.toJson(response));
+            log.info("GreetingDubboRpcService sayHello, name:{}, response:{}", name, JsonUtils.toJson(response));
             if (Objects.isNull(response)
                     || Objects.isNull(response.getData())
                     || BaseResponse.isError(response)) {
-                log.error("GreetingDubboRpcService sayHello, name:{}, response has error" , name, new RpcException("response has error"));
+                log.error("GreetingDubboRpcService sayHello, name:{}, response has error", name, new RpcException("response has error"));
                 return null;
             }
             return response.getData();
         } catch (Exception e) {
-            log.error("GreetingDubboRpcService sayHello, name:{}" , name, new RpcException(e));
+            log.error("GreetingDubboRpcService sayHello, name:{}", name, new RpcException(e));
             return null;
         }
     }
