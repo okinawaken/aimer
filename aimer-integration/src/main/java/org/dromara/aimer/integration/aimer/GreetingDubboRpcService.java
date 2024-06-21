6e06f5bbc3ec3681a0d5045f8837ec4c42907aff
Shinomiya <1138700280@qq.com>
2024-06-21 15:40:18 +0800

    使用dubbo

--- aimer-integration/src/main/java/org/dromara/aimer/integration/aimer/GreetingDubboRpcService.java
+++ aimer-integration/src/main/java/org/dromara/aimer/integration/aimer/GreetingDubboRpcService.java
@@ -6,6 +6,7 @@ import org.dromara.aimer.common.exception.RpcException;
 import org.dromara.aimer.common.response.BaseResponse;
 import org.dromara.aimer.common.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -18,6 +19,7 @@ import java.util.Objects;
 public class GreetingDubboRpcService {
 
     @Autowired
+    @Qualifier("greetingRpcService")
     private IGreetingRpcService greetingRpcService;
 
     public String sayHello(String name) {
