a4093db5274b088d99f71ee1fa72cc138746c851
Shinomiya <1138700280@qq.com>
2024-06-24 19:43:05 +0800

    简化开发

--- aimer-auth/src/main/java/org/dromara/aimer/auth/service/auth/impl/AuthServiceImpl.java
+++ aimer-auth/src/main/java/org/dromara/aimer/auth/service/auth/impl/AuthServiceImpl.java
@@ -1,11 +1,7 @@
 package org.dromara.aimer.auth.service.auth.impl;
 
-import org.apache.commons.lang3.BooleanUtils;
 import org.dromara.aimer.api.dto.RegisterBodyDTO;
 import org.dromara.aimer.auth.service.auth.IAuthService;
-import org.dromara.aimer.common.exception.UserException;
-import org.dromara.aimer.integration.aimer.SystemConfigDubboRpcService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,14 +10,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthServiceImpl implements IAuthService {
 
-    @Autowired
-    private SystemConfigDubboRpcService systemConfigDubboRpcService;
-
     @Override
     public void register(RegisterBodyDTO registerBodyDTO) {
-        Boolean isRegisterEnable = systemConfigDubboRpcService.queryRegisterEnable(registerBodyDTO.getTenantId());
-        if (BooleanUtils.isNotFalse(isRegisterEnable)) {
-            throw new UserException("当前系统注册未开启");
-        }
+
     }
 }
