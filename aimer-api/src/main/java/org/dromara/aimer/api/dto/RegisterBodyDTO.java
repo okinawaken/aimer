e90b8489ec0945b85e8f1882a3256eb552236276
Shinomiya <1138700280@qq.com>
2024-06-24 20:24:41 +0800

    validator

--- aimer-api/src/main/java/org/dromara/aimer/api/dto/RegisterBodyDTO.java
+++ aimer-api/src/main/java/org/dromara/aimer/api/dto/RegisterBodyDTO.java
@@ -13,8 +13,22 @@ import lombok.NoArgsConstructor;
 public class RegisterBodyDTO {
 
     /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    /**
+     * 用户类型
+     */
+    private String userType;
+
+    /**
      * 租户ID
      */
     private String tenantId;
-
 }
---
+++ aimer-auth/src/main/java/org/dromara/aimer/auth/service/auth/validator/AuthValidator.java
@@ -0,0 +1,13 @@
+package org.dromara.aimer.auth.service.auth.validator;
+
+import org.dromara.aimer.api.dto.RegisterBodyDTO;
+
+/**
+ * @author bixiu
+ */
+public class AuthValidator {
+
+    public void validateRegister(RegisterBodyDTO registerBodyDTO) {
+
+    }
+}
