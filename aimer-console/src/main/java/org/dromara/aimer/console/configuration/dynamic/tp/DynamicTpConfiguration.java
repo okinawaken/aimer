118571dd82eb2c2a3d93d702a24e1a5da1355a5b
Shinomiya <1138700280@qq.com>
2024-06-24 16:42:04 +0800

    引入dynamic-tp线程池

---
+++ aimer-console/src/main/java/org/dromara/aimer/console/configuration/dynamic/tp/DynamicTpConfiguration.java
@@ -0,0 +1,12 @@
+package org.dromara.aimer.console.configuration.dynamic.tp;
+
+import org.dromara.dynamictp.core.spring.EnableDynamicTp;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author bixiu
+ */
+@EnableDynamicTp
+@Configuration
+public class DynamicTpConfiguration {
+}
