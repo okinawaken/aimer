760a02fcc18f33fbbadbc066642c1685b3b9eb35
Shinomiya <1138700280@qq.com>
2024-06-26 17:13:28 +0800

    添加author

--- aimer-console/src/main/java/org/dromara/aimer/console/controller/generator/MybatisPlusGeneratorController.java
+++ aimer-console/src/main/java/org/dromara/aimer/console/controller/generator/MybatisPlusGeneratorController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author bixiu
+ */
 @RestController
 @RequestMapping("/mybatis-plus-generator")
 public class MybatisPlusGeneratorController {
